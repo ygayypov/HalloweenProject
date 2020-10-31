fbdf4183f71d08c66f882ae5d523b27705ed246d
ygayypov <68933447+ygayypov@users.noreply.github.com>
2020-10-31 12:39:26 -0600

    make a few change

--- src/main/java/featureA_Code.java
+++ src/main/java/featureA_Code.java
@@ -2,6 +2,7 @@ public class featureA_Code {
 
     public static void main(String[] args) {
         System.out.println("Awesome featureA is in progress");
+        System.out.println("Good to see you here");
     }
 
 }
