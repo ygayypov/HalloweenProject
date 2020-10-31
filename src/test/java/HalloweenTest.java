20a27d72f57b7284ae7bfeb6535c37c791d39330
ygayypov <68933447+ygayypov@users.noreply.github.com>
2020-10-31 10:43:47 -0600

    new change in trick

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -5,5 +5,8 @@ public class HalloweenTest {
     //lets play a game
     //its fun to drive so far
 
+    //there will be no treat and trick this year
+    //stay home and be safe and wear your mask outside
+
 
 }
